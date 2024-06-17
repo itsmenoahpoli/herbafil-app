24fea315e2742504cd306a929eb78e0152efb258
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-06-17 14:43:59 +0800

    update

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
-import { BrandLogo, SigninForm } from "@/components";
+import { BrandLogo } from "@/components/shared";
+import { SigninForm } from "@/components/domains/auth";
 import { BaseLayout } from "@/layouts";
 import type { TStackParamsList } from "@/types/navigation";
 
