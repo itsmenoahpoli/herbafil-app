5e3ff76e1b559912ccceafc3a9f430e72cf66f44
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-06-18 03:56:13 +0800

    update

--- src/screens/PlantCaptureScreen.tsx
+++ src/screens/PlantCaptureScreen.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
+import { AntDesign } from "@expo/vector-icons";
 import { BrandLogo } from "@/components/shared";
 import { BaseLayout } from "@/layouts";
 import type { TStackParamsList } from "@/types/navigation";
@@ -13,6 +14,14 @@ export const PlantCaptureScreen: React.FC<TScreenProps> = (props) => {
   return (
     <BaseLayout>
       <View className="flex-1 items-center pt-[5%]">
+        <Text className="text-xl text-green-900 text-center font-bold">Capture Plant</Text>
+
+        <View className="w-full flex-1 border mt-10"></View>
+
+        <Pressable className="bg-purple-700 border border-white rounded-full p-4 mt-10">
+          <AntDesign name="camerao" size={36} color="white" />
+        </Pressable>
+
         <BrandLogo />
       </View>
     </BaseLayout>
